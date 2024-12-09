ec1c8d72b806a1d32c0e5d22204441b54e73ff3b
saket kumar keshri <keshrisaket414@gmail.com>
2024-12-09 14:30:49 +0530

    Feature Addition:
    "Add separate authentication providers for SuperAdmin , Admin and Student roles"
    "Implement role-based access control for SuperAdmin , Admin and Student endpoints"
    
    Bug Fix:
    
    "Fix issue with authentication logic for SuperAdmin users"
    "Resolve conflict between UserDetailsService implementations"
    
    Refactor:
    
    "Refactor security configuration to support multiple user roles"
    "Simplify authentication logic by creating dedicated AuthenticationProviders"

--- src/main/java/com/question_bank_backend/semester/SemesterController.java
+++ src/main/java/com/question_bank_backend/semester/SemesterController.java
@@ -1,8 +1,14 @@
 package com.question_bank_backend.semester;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.question_bank_backend.exceptions.CourseNotFoundException;
+import com.question_bank_backend.exceptions.SemesterNotFoundException;
+import com.question_bank_backend.utility.MyResponseHandler;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "${api.prefix}/semesters}")
@@ -14,5 +20,79 @@ public class SemesterController {
         this.semesterService = semesterService;
     }
 
+
+    @GetMapping("/semester/by/course-full-name")
+    public ResponseEntity<Object> getAllSemestersByCourseFullName(@RequestParam String courseFullName) {
+        try {
+            List<SemesterEntity> semesterEntities = semesterService.getAllSemestersByCourseFullName(courseFullName);
+            return MyResponseHandler.generateResponse(HttpStatus.OK, false, "Found!", semesterEntities);
+        } catch (CourseNotFoundException e) {
+            return MyResponseHandler.generateResponse(HttpStatus.NOT_FOUND, true, e.getMessage(), null);
+        }
+    }
+
+
+    @GetMapping("/semester/by/course-short-name")
+    public ResponseEntity<Object> getAllSemesterByCourseShortName(@RequestParam String courseShortName) {
+        try {
+            List<SemesterEntity> semesterEntities = semesterService.getAllSemestersByCourseShortName(courseShortName);
+            return MyResponseHandler.generateResponse(HttpStatus.OK, false, "found!", semesterEntities);
+        } catch (CourseNotFoundException e) {
+            return MyResponseHandler.generateResponse(HttpStatus.NOT_FOUND, true, e.getMessage(), null);
+        }
+    }
+
+    @GetMapping("/semester/{courseId}/all")
+    public ResponseEntity<Object> getAllSemesterByCourseId(@PathVariable String courseId) {
+        try {
+            List<SemesterEntity> semesterEntities = semesterService.getAllSemestersByCourseId(courseId);
+            return MyResponseHandler.generateResponse(HttpStatus.OK, false, "found!", semesterEntities);
+        } catch (CourseNotFoundException e) {
+            return MyResponseHandler.generateResponse(HttpStatus.NOT_FOUND, true, e.getMessage(), null);
+        }
+    }
+
+    @PutMapping("/semester/{id}/update")
+    public ResponseEntity<Object> updateSemester(@PathVariable String id, @RequestBody SemesterDto semesterDto) {
+        try {
+            SemesterEntity semesterEntity = semesterService.updateSemesterBySemesterId(semesterDto, id);
+            return MyResponseHandler.generateResponse(HttpStatus.OK, false, "Updated!", semesterEntity);
+        } catch (SemesterNotFoundException e) {
+            return MyResponseHandler.generateResponse(HttpStatus.NOT_FOUND, true, e.getMessage(), null);
+        }
+    }
+
+    @GetMapping("/semester/{id}/semester")
+    public ResponseEntity<Object> getSemesterById(@PathVariable String id) {
+        try {
+            SemesterEntity semesterEntity = semesterService.getSemesterBySemesterId(id);
+            return MyResponseHandler.generateResponse(HttpStatus.OK, false, "found!", semesterEntity);
+        } catch (SemesterNotFoundException e) {
+            return MyResponseHandler.generateResponse(HttpStatus.NOT_FOUND, true, e.getMessage(), null);
+        }
+    }
+
+    @DeleteMapping("/semester/{id}/delete")
+    public ResponseEntity<Object> deleteSemesterSemesterId(@PathVariable String id) {
+        try {
+            semesterService.deleteSemesterBySemesterId(id);
+            return MyResponseHandler.generateResponse(HttpStatus.OK, false, "Deleted!", null);
+        } catch (SemesterNotFoundException e) {
+            return MyResponseHandler.generateResponse(HttpStatus.NOT_FOUND, true, e.getMessage(), null);
+        }
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Object> addSemester(@RequestBody SemesterDto semesterDto) {
+        try{
+            if (semesterDto!=null){
+                SemesterEntity semesterEntity = semesterService.addSemester(semesterDto);
+                return MyResponseHandler.generateResponse(HttpStatus.OK, false, "Added!", semesterEntity);
+            }
+        }catch (Exception e){
+            return MyResponseHandler.generateResponse(HttpStatus.INTERNAL_SERVER_ERROR, true, "Error!", null);
+        }
+        return MyResponseHandler.generateResponse(HttpStatus.INTERNAL_SERVER_ERROR, true, "Error!", null);
+    }
 
 }
--- src/main/java/com/question_bank_backend/subject/SubjectController.java
+++ src/main/java/com/question_bank_backend/subject/SubjectController.java
@@ -5,9 +5,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "")
-public class SubjectController
-{
+@RequestMapping(path = "${api.prefix}/subjects")
+public class SubjectController {
+
+
+    private final SubjectService subjectservice;
+
+    public SubjectController(SubjectService subjectService) {
+        this.subjectservice = subjectService;
+    }
+
+
+
+
+
 
 
 }
